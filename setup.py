43de36a3506a454f5045878d27ec30c346f90fef
DerThorsten <derthorsten@gmail.com>
2019-08-22 00:11:12 +0000

    Bump version: 0.1.37 → 0.1.38

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DerThorsten/py_tools',
-    version='0.1.37',
+    version='0.1.38',
     zip_safe=False,
 )
