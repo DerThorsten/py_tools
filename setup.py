589ae434bc453bc54ec7072bda1d3a69f73a5be5
DerThorsten <derthorsten@gmail.com>
2019-09-25 00:07:38 +0000

    Bump version: 0.1.71 → 0.1.72

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DerThorsten/py_tools',
-    version='0.1.71',
+    version='0.1.72',
     zip_safe=False,
 )
--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ class TestVersion(object):
 
     def test_version(self):
         v = py_tools.__version__
-        assert v == '0.1.71'
+        assert v == '0.1.72'
