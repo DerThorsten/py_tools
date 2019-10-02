b17f42598174a088731df108b34d1e82f8f30036
DerThorsten <derthorsten@gmail.com>
2019-10-02 00:04:38 +0000

    Bump version: 0.1.78 → 0.1.79

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DerThorsten/py_tools',
-    version='0.1.78',
+    version='0.1.79',
     zip_safe=False,
 )
