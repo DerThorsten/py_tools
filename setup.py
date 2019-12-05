137992aee5369f5a0eb4012413e6ad242acc640c
DerThorsten <derthorsten@gmail.com>
2019-12-05 00:06:29 +0000

    Bump version: 0.1.142 → 0.1.143

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DerThorsten/py_tools',
-    version='0.1.142',
+    version='0.1.143',
     zip_safe=False,
 )
